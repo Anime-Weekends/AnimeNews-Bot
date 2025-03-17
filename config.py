3082a67413a9836726d2cf0f1c73094596163512
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2025-03-17 21:40:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 
-API_ID = "" #API ID get it from telegram.org
-API_HASH = "" #API HASH get it from telegram.org
-BOT_TOKEN = "" #Bot Token get it from @BotFather
-URL_A = "" #News RSS Feed Url 2
+API_ID = "28744454" #API ID get it from telegram.org
+API_HASH = "debd37cef0ad1a1ce45d0be8e8c3c5e7" #API HASH get it from telegram.org
+BOT_TOKEN = "7684644640:AAFzdKIaCmwbhkQFT5aJ12oorOOLntVtKR0" #Bot Token get it from @BotFather
+URL_A = "https://feeds.feedburner.com/crunchyroll/rss" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
-START_PIC = "" #Startup Pic
-MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
+START_PIC = https://i.ibb.co/YBthP0Y1/photo-2025-03-17-16-08-10-7482811126829285400.jpg"" #Startup Pic
+MONGO_URI = "mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #Databse uri get it from https://www.mongodb.com/
