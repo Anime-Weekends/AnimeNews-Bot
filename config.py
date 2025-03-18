04d5fcc7de01de42fa152cfcad46b8c5d765bcb2
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2025-03-18 23:56:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 API_ID = "28744454" #API ID get it from telegram.org
 API_HASH = "debd37cef0ad1a1ce45d0be8e8c3c5e7" #API HASH get it from telegram.org
 BOT_TOKEN = "7684644640:AAFzdKIaCmwbhkQFT5aJ12oorOOLntVtKR0" #Bot Token get it from @BotFather
-URL_A = "https://www.theanimedaily.com/feed/" #News RSS Feed Url 2
+URL_A = "https://myanimelist.net/" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
 START_PIC = "tohttps://i.ibb.co/ZRgXxPdC/photo-2025-03-18-18-24-32-7483217203102220292.jpg" #Startup Pic
 MONGO_URI = "mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority" #Databse uri get it from https://www.mongodb.com/
