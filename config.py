a37874f11ca0a12343d2221dd2cd27ab746f7d54
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2025-03-19 11:03:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 API_ID = "28744454" #API ID get it from telegram.org
 API_HASH = "debd37cef0ad1a1ce45d0be8e8c3c5e7" #API HASH get it from telegram.org
 BOT_TOKEN = "7684644640:AAFzdKIaCmwbhkQFT5aJ12oorOOLntVtKR0" #Bot Token get it from @BotFather
-URL_A = "https://myanimelist.net/" #News RSS Feed Url 2
+URL_A = "https://feeds.feedburner.com/crunchyroll/rss" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
 START_PIC = "https://i.ibb.co/xqz1cS2Y/photo-2025-03-18-18-32-32-7483219264686522396.jpg" #Startup Pic
 MONGO_URI = "mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority" #Databse uri get it from https://www.mongodb.com/
